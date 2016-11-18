dbd9a423234a54c85c5d09b95a4fce3532387d8d
Brendan Pete <brendan.m.pete@gmail.com>
2016-11-17 22:13:52 -0500

    Adding dist folder

---
+++ dist/js/app.js
@@ -0,0 +1,140 @@
+var words = [
+        'Dooickeys',
+        'Pseghting',
+        'Moqnuqeek',
+        'Poncicles',
+        'Bamanapan',
+        'Groindogs',
+        'Lorfickle',
+        'Smoomcake',
+        'Limgistic',
+        'Mostaming',
+        'Dreamdump',
+        'Miltmirks',
+        'Dangmurse',
+        'Dosmingle'
+    ],
+    totalWords = words.length,
+    playedWords = [],
+    player = {
+        'one': {
+            'word': [],
+            'progress': 0,
+            'score': 0,
+            'winner': false
+        },
+        'two': {
+            'word': [],
+            'progress': 0,
+            'score': 0,
+            'winner': false
+        }
+    };
+
+function watchKeys() {
+    $('body').keypress(function(input) {
+        battle(input);
+    });
+}
+
+function keepScore () {
+    $('#js-p1-score').text(player.one.score);
+    $('#js-p2-score').text(player.two.score);
+}
+
+function getWinner() {
+    if (playedWords.length === totalWords) {
+        if (player.one.score > player.two.score) {
+            console.log('p1 - wins the game')
+        } else if (player.one.score > player.two.score) {
+            console.log('p1 - wins the game')
+        }
+    }
+}
+
+function shuffle(array) {
+    var m = array.length, t, i;
+    while (m) {
+        i = Math.floor(Math.random() * m--);
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+    return array;
+}
+
+function addWords() {
+    player.one.word = words[0].split('');
+    playedWords.push(words[0]);
+    player.two.word = words[1].split('');
+    playedWords.push(words[1]);
+    words.splice(0, 2);
+
+    $('#js-p1-wordlist').prepend(
+        '<li><h2>' + player.one.word.join('') + '</h2></li>'
+    );
+    $('#js-p2-wordlist').prepend(
+        '<li><h2>' + player.two.word.join('') + '</h2></li>'
+    );
+}
+
+function battle(input) {
+    if (input.key === player.one.word[0] && input.key === player.two.word[0]) {
+        if (player.one.progress === player.two.progress){
+            player.one.progress = player.one.progress + 1;
+            player.two.progress = player.two.progress + 1;
+            player.one.word.shift();
+            player.two.word.shift();
+            console.log('p1 & p2 - matched (both advance)', player.one.word, player.two.word);
+        } else if (player.one.progress < player.two.progress) {
+            player.one.progress = player.one.progress + 1;
+            player.one.word.shift();
+            console.log('p1 & p2 - matched (player 1 advances)', player.one.word, player.two.word);
+        } else {
+            player.two.progress = player.two.progress + 1;
+            player.two.word.shift();
+            console.log('p1 & p2 - matched (player 2 advances)', player.one.word, player.two.word);
+        }
+    } else if (input.key === player.one.word[0]){
+        player.one.progress = player.one.progress + 1;
+        player.one.word.shift();
+        console.log('p1 - matched', player.one.word)
+    } else if (input.key === player.two.word[0]){
+        player.two.progress = player.two.progress + 1;
+        player.two.word.shift();
+        console.log('p2 - matched', player.two.word)
+    }
+    getRoundWinner();
+}
+
+function getRoundWinner() {
+    if (player.one.word.length === 0) {
+        player.one.score = player.one.score + 1;
+        startRound();
+        console.log('p1 - wins round');
+    } else if (player.two.word.length === 0) {
+        player.two.score = player.two.score + 1;
+        startRound();
+        console.log('p2 - wins round');
+    } else if (totalWords === playedWords.length) {
+        console.log('game over')
+    }
+    keepScore();
+}
+
+function startRound() {
+    shuffle(words);
+    addWords();
+    watchKeys();
+    getWinner();
+}
+
+function init() {
+    keepScore();
+    startRound();
+}
+
+$(document).ready(function() {
+    init();
+});
+

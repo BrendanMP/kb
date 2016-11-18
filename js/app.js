7eab0804a6be39e53cc434d47c111a21541dcaf8
Brendan Pete <brendan.m.pete@gmail.com>
2016-11-18 17:37:23 -0500

    Updated app.js

--- js/app.js
+++ js/app.js
@@ -2,9 +2,9 @@ var words = [
         'dog',
         'cat',
         'fox',
-        'bug',
-        'pup',
-        'emu'
+        'bug'
+        //'pup',
+        //'emu'
     ],
     wordTotal = words.length,
     playedWords = [],
@@ -21,12 +21,20 @@ var words = [
         }
     };
 
+// Watch for key input.
+function watchKeys() {
+    $('body').keypress(function (input) {
+        doBattle(input);
+    });
+}
 
+// Updated the scores.
 function updateScores() {
     $('#js-p1-score').text(player.one.score);
     $('#js-p2-score').text(player.two.score);
 }
 
+// Add words to play.
 function addWords() {
     player.one.word = words[0].split('');
     player.two.word = words[1].split('');
@@ -40,6 +48,7 @@ function addWords() {
     );
 }
 
+// Determine who advances.
 function doBattle(input) {
     // P1 or P2 advances to next letter next after matched input.
     // If P1 and P2 share progress & active character they both advance.
@@ -50,68 +59,78 @@ function doBattle(input) {
             player.two.progress = player.two.progress + 1;
             player.one.word.shift();
             player.two.word.shift();
-            console.log('P1 & P2 - matched (both advance)', player.one.word, player.two.word);
+            console.log('P1 & P2 - Matched (Both Advance)', player.one.word, player.two.word);
         } else if (player.one.progress < player.two.progress) {
             player.one.progress = player.one.progress + 1;
             player.one.word.shift();
-            console.log('P1 & P2 - matched (player 1 advances)', player.one.word, player.two.word);
+            console.log('P1 & P2 - Matched (Player 1 Advances)', player.one.word, player.two.word);
         } else {
             player.two.progress = player.two.progress + 1;
             player.two.word.shift();
-            console.log('P1 & P2 - matched (player 2 advances)', player.one.word, player.two.word);
+            console.log('P1 & P2 - Matched (Player 2 Advances)', player.one.word, player.two.word);
         }
     } else if (input.key === player.one.word[0]) {
         player.one.progress = player.one.progress + 1;
         player.one.word.shift();
-        console.log('P1 - matched', player.one.word)
+        console.log('P1 - Matched', player.one.word)
     } else if (input.key === player.two.word[0]) {
         player.two.progress = player.two.progress + 1;
         player.two.word.shift();
-        console.log('P2 - matched', player.two.word)
+        console.log('P2 - Matched', player.two.word)
     }
     roundWinner();
 }
 
+// Determine round winner.
 function roundWinner() {
     if (player.one.word.length === 0) {
-        console.log('P1 wins round');
-        words.splice(0, 2);
         player.one.score = player.one.score + 1;
-        startRound();
-    } else if (player.two.word.length === 0) {
-        console.log('P2 wins round');
+        console.log('P1 - Wins Round');
         words.splice(0, 2);
+        if (words.length === 0) {
+            gameOver();
+        } else {
+            startRound();
+        }
+    } else if (player.two.word.length === 0) {
         player.two.score = player.two.score + 1;
-        startRound();
+        console.log('P2 - Wins Round');
+        words.splice(0, 2);
+        if (words.length === 0) {
+            gameOver();
+        } else {
+            startRound();
+        }
     }
     updateScores();
 }
 
+// Start a round.
 function startRound() {
-    if (playedWords.length < wordTotal) {
-        addWords();
-        $('body').keypress(function (input) {
-            doBattle(input);
-        });
-    } else {
-        gameOver();
-    }
+    addWords();
+    watchKeys();
 }
 
+// Game over.
 function gameOver() {
     console.log('game over');
     if (player.one.score > player.two.score) {
         console.log('P1 Wins Game')
-    } else if ( player.one.score < player.two.score ) {
+    } else if (player.one.score < player.two.score) {
         console.log('P2 Wins Game')
     }
 }
 
+// Start the game.
 function startGame() {
+    // Init Scores
     updateScores();
+    // Start Rounds
     startRound();
 }
 
+// Ready the DOM for BATTLE.
 $(document).ready(function () {
+    // Start game.
     startGame();
 });

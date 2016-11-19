fbbaa04daf61eaca1fc448185412e855086a1351
Brendan Pete <brendan.m.pete@gmail.com>
2016-11-18 19:46:40 -0500

    Added round winner message

--- js/app.js
+++ js/app.js
@@ -107,10 +107,12 @@ function doBattle(input) {
 function roundWinner() {
     if (player.one.word.length === 0) {
         player.one.score = player.one.score + 1;
+        $('#console').text('Player One Wins Round');
         console.log('P1 - Wins Round');
         startRound();
     } else if (player.two.word.length === 0) {
         player.two.score = player.two.score + 1;
+        $('#console').text('Player Two Wins Round');
         console.log('P2 - Wins Round');
         startRound();
     }
